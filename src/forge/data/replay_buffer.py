98135d45d883c7ea524b03c38e2c447b5345b2b4
joecummings <jrcummings27@gmail.com>
2025-07-31 14:18:01 -0700

    State for random sampling and sample while removing trajectories

--- src/forge/data/replay_buffer.py
+++ src/forge/data/replay_buffer.py
@@ -4,20 +4,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-"""
-Initialize, add, sample, size/len, evict, clear
-Uniform sampling (without replacement)
-Batching
-Able to restart training from exact location
-Reproducible
-Proposed implementation
-Python list to back the buffer
-Evict based on policy_version on a Rollout (essentially just if a Rollout is too off policy, we get rid of it)
-Simple collation function for batching, not exposed directly to user since only doing text data
-Set RNG in sampler, exposed to user
-Stateful functionality for resuming from failure
-"""
-
 import random
 from typing import Any
 
@@ -30,11 +16,15 @@ from torchdata.stateful_dataloader import Stateful
 class ReplayBuffer(Actor, Stateful):
     """Simple in-memory replay buffer implementation."""
 
-    def __init__(self, batch_size: int, max_policy_age: int) -> None:
+    def __init__(
+        self, batch_size: int, max_policy_age: int, seed: int | None = None
+    ) -> None:
         self.buffer: list[Trajectory] = []
         self.batch_size = batch_size
         self.max_policy_age = max_policy_age
-        self.sampler = random.choices
+        if seed is not None:
+            random.seed(seed)
+        self.sampler = random.sample
 
     @endpoint
     async def add(self, trajectory: Trajectory) -> None:
@@ -61,7 +51,13 @@ class ReplayBuffer(Actor, Stateful):
 
         await self.evict(curr_policy_version)
 
-        return self.sampler(self.buffer, k=bsz)
+        # TODO: Make this more efficient
+        idx_to_sample = self.sampler(range(len(self.buffer)), k=bsz)
+        sorted_idxs = sorted(
+            idx_to_sample, reverse=True
+        )  # Sort in desc order to avoid shifting idxs
+        sampled_trajectories = [self.buffer.pop(i) for i in sorted_idxs]
+        return sampled_trajectories
 
     @endpoint
     async def evict(self, curr_policy_version: int) -> None:
@@ -85,7 +81,11 @@ class ReplayBuffer(Actor, Stateful):
         self.buffer.clear()
 
     def state_dict(self) -> dict[str, Any]:
-        return {"buffer": self.buffer}
+        return {
+            "buffer": self.buffer,
+            "rng_state": random.getstate(),
+        }
 
     def load_state_dict(self, state_dict: dict[str, Any]) -> None:
         self.buffer = state_dict["buffer"]
+        random.setstate(state_dict["rng_state"])
