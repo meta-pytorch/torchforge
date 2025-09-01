fc5c84308cbce68f4fa2ff216a8567a6d701a5a9
Yuxuan Hu <yuxuanh@meta.com>
2025-09-01 02:11:09 -0700

    clean up

--- apps/grpo/main.py
+++ apps/grpo/main.py
@@ -26,9 +26,6 @@ from forge.util.metric_logging import get_metric_logger
 from monarch.actor import endpoint
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from transformers.models.granitemoehybrid.modeling_granitemoehybrid import (
-    apply_mask_to_padding_states,
-)
 
 FLAGS = flags.FLAGS
 
