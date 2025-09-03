8d38eb865b3f7c92c6edcadb72d081566e140190
DNXie <dxie@meta.com>
2025-09-03 16:06:22 -0700

    lint

--- tests/unit_tests/test_policy_config.py
+++ tests/unit_tests/test_policy_config.py
@@ -4,10 +4,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import inspect
 import tempfile
 import unittest
-from dataclasses import asdict
 
 import yaml
 
