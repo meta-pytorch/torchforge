e0b5eae71793a94b4647b0eb2a006fdef56be58d
pradeepfn <pradeepfn@gmail.com>
2025-09-18 18:46:05 -0700

    code format

--- src/forge/actors/policy.py
+++ src/forge/actors/policy.py
@@ -16,12 +16,6 @@ from dataclasses import asdict, dataclass, field, fields
 
 import torch
 import torchstore as ts
-
-from forge.controller import ForgeActor, get_proc_mesh, stop_proc_mesh
-
-from forge.data.sharding import VLLMSharding
-from forge.interfaces import Policy as PolicyInterface
-from forge.types import ProcessConfig
 from monarch.actor import current_rank, endpoint, ProcMesh
 from torchstore.state_dict_utils import DELIM
 from vllm.config import VllmConfig
@@ -45,6 +39,12 @@ from vllm.v1.engine.processor import Processor
 from vllm.v1.request import Request
 from vllm.v1.structured_output import StructuredOutputManager
 from vllm.worker.worker_base import WorkerWrapperBase
+
+from forge.controller import ForgeActor, get_proc_mesh, stop_proc_mesh
+
+from forge.data.sharding import VLLMSharding
+from forge.interfaces import Policy as PolicyInterface
+from forge.types import ProcessConfig
 
 
 @dataclass
