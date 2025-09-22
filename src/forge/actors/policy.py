e0a11e8d11d40b63225b1b76187616cafcfa314d
rithesh <rithesh@devgpu001.lla1.facebook.com>
2025-09-22 10:33:59 -0700

    lints

--- src/forge/actors/policy.py
+++ src/forge/actors/policy.py
@@ -16,14 +16,6 @@ from dataclasses import asdict, dataclass, field, fields
 
 import torch
 import torchstore as ts
-
-from forge.controller import ForgeActor, get_proc_mesh, stop_proc_mesh
-
-from forge.data.sharding import VLLMSharding
-from forge.data_models.completion import Completion
-from forge.data_models.prompt import Prompt, to_prompt
-from forge.interfaces import Policy as PolicyInterface
-from forge.types import ProcessConfig
 from monarch.actor import current_rank, endpoint, ProcMesh
 from torchstore.state_dict_utils import DELIM
 from vllm.config import VllmConfig
@@ -47,6 +39,14 @@ from vllm.v1.engine.processor import Processor
 from vllm.v1.request import Request
 from vllm.v1.structured_output import StructuredOutputManager
 from vllm.worker.worker_base import WorkerWrapperBase
+
+from forge.controller import ForgeActor, get_proc_mesh, stop_proc_mesh
+
+from forge.data.sharding import VLLMSharding
+from forge.data_models.completion import Completion
+from forge.data_models.prompt import to_prompt
+from forge.interfaces import Policy as PolicyInterface
+from forge.types import ProcessConfig
 
 
 @dataclass
