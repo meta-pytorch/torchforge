c39eb8953f5a9355d90e8398d8f0acb59b46d3f9
rithesh <rithesh@devgpu001.lla1.facebook.com>
2025-09-26 10:25:54 -0700

    few more changes

--- src/forge/actors/policy.py
+++ src/forge/actors/policy.py
@@ -163,6 +163,7 @@ class Policy(PolicyInterface):
         engine_config: EngineConfig | Mapping = EngineConfig(),
         sampling_config: SamplingConfig | Mapping = SamplingConfig(),
         available_devices: str | None = None,
+        checkpoint_path: str | None = None,
         **kwargs,
     ) -> "Policy":
         # Note - get_proc_mesh will set MASTER_ADDR, MASTER_PORT and CUDA_VISIBLE_DEVICES
@@ -194,7 +195,10 @@ class Policy(PolicyInterface):
 
         vllm_config = engine_config.create_vllm_config()
         workers = await worker_procs.spawn(
-            "vllm_worker", PolicyWorker, vllm_config=vllm_config
+            "vllm_worker",
+            PolicyWorker,
+            vllm_config=vllm_config,
+            checkpoint_path=checkpoint_path,
         )
 
         if isinstance(sampling_config, Mapping):
@@ -486,6 +490,9 @@ class PolicyWorker(ForgeActor):
     # TODO: remove this later since no plumbing exists to change this value.
     # Also, whether to use dcp or not can be inferred from torchstore get() call.
     use_dcp: bool = True
+    # Cache hf param names on first update call.
+    hf_param_names = []
+    checkpoint_path: str | None = None
 
     # used for tesing purposes only
     _test_prev_params = {}
@@ -514,6 +521,10 @@ class PolicyWorker(ForgeActor):
         )
 
         checkpoint_id = f"{self.state_dict_key}{DELIM}{version}"
+        if self.checkpoint_path is not None and self.use_dcp:
+            checkpoint_id = (
+                f"{self.checkpoint_path}{DELIM}{self.state_dict_key}{DELIM}{version}"
+            )
         dcp_metadata = None
         if self.use_dcp:
             dcp_metadata = await ts.get(checkpoint_id)
