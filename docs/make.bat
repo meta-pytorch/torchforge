8e47bc505a88929e29f2de0f6c2c5101d48818ff
Joseph Spisak <spisakjo@gmail.com>
2025-09-30 11:31:20 -0700

    Update make.bat

--- docs/make.bat
+++ docs/make.bat
@@ -4,8 +4,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-@ECHO OFF
-
 pushd %~dp0
 
 REM Command file for Sphinx documentation
@@ -39,4 +37,5 @@ goto end
 
 :end
 popd
+
 
