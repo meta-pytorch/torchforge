e6d6f6b3da8a690c93020be1a8412bf201ce1fc1
Joseph Spisak <spisakjo@gmail.com>
2025-09-30 11:25:23 -0700

    Update make.bat
    Added license header to this file

--- docs/make.bat
+++ docs/make.bat
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 @ECHO OFF
 
 pushd %~dp0
@@ -33,3 +39,4 @@ goto end
 
 :end
 popd
+
