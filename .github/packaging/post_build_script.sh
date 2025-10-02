9d100819e6752d711033a0ce4fa0bf011d5234d5
Evan Smothers <ebs@fb.com>
2025-10-02 11:36:24 -0700

    recreate wheel build PR

---
+++ .github/packaging/post_build_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euxo pipefail
+
+FORGE_WHEEL=${GITHUB_WORKSPACE}/${REPOSITORY}/dist/*.whl
+WHL_DIR="${GITHUB_WORKSPACE}/wheels/"
+DIST=dist/
+
+echo "Uploading wheels to S3"
+ls -l "${WHL_DIR}"
+ls ${FORGE_WHEEL}
+echo "Copying files from $WHL_DIR to $DIST"
+mkdir -p $DIST && cp $WHL_DIR/* $DIST
