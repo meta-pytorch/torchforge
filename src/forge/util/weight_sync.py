e66efee33e8e8a943b9dfde2ec8dc65a781836b2
Yuxuan Hu <yuxuanh@meta.com>
2025-10-02 13:28:42 -0700

    import time

--- src/forge/util/weight_sync.py
+++ src/forge/util/weight_sync.py
@@ -4,6 +4,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+import time
+
 import torchstore as ts
 
 from forge.actors._torchstore_utils import (
