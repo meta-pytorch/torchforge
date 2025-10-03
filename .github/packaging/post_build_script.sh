ecc1dc05e8b0191a3b3b7066510e258f3aa7d0a2
Evan Smothers <ebs@fb.com>
2025-10-03 13:27:45 -0700

    no rsync

--- .github/packaging/post_build_script.sh
+++ .github/packaging/post_build_script.sh
@@ -9,5 +9,5 @@ echo "Uploading wheels to S3"
 ls -l "${WHL_DIR}"
 ls ${FORGE_WHEEL}
 echo "Copying files from $WHL_DIR to $DIST"
-mkdir -p $DIST && rsync -r --exclude="/*/" $WHL_DIR/ $DIST/
+mkdir -p $DIST && find "$WHL_DIR" -maxdepth 1 -type f -exec cp {} "$DIST/" \;
 ls -l "${DIST}"
