2802de7593813df64608b28acf16c8f07a85e01e
Evan Smothers <ebs@fb.com>
2025-10-03 14:31:46 -0700

    address comments

--- .github/packaging/post_build_script.sh
+++ .github/packaging/post_build_script.sh
@@ -5,9 +5,9 @@ FORGE_WHEEL=${GITHUB_WORKSPACE}/${REPOSITORY}/dist/*.whl
 WHL_DIR="${GITHUB_WORKSPACE}/wheels/dist"
 DIST=dist/
 
-echo "Uploading wheels to S3"
 ls -l "${WHL_DIR}"
 ls ${FORGE_WHEEL}
 echo "Copying files from $WHL_DIR to $DIST"
 mkdir -p $DIST && find "$WHL_DIR" -maxdepth 1 -type f -exec cp {} "$DIST/" \;
+echo "The following wheels will be uploaded to S3"
 ls -l "${DIST}"
