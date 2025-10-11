e47493c401f573238af28a6c3cd5ae4d6752ed83
Yuxuan Hu <yuxuanh@meta.com>
2025-10-11 12:39:30 -0700

    sequential put

--- src/forge/actors/trainer.py
+++ src/forge/actors/trainer.py
@@ -19,6 +19,17 @@ import torch
 import torch.distributed.checkpoint as dcp
 import torchstore as ts
 
+from forge.actors._torchstore_utils import (
+    DcpHandle,
+    get_dcp_whole_state_dict_key,
+    get_param_key,
+)
+
+from forge.controller import ForgeActor
+from forge.data.utils import batch_to_device
+from forge.observability.metrics import record_metric, Reduce
+from forge.observability.perf_tracker import Tracer
+
 from monarch.actor import current_rank, current_size, endpoint
 from torch import Tensor
 from torch.distributed.checkpoint._nested_dict import flatten_state_dict
@@ -38,13 +49,6 @@ from torchtitan.config.job_config import (
 )
 from torchtitan.experiments.forge.engine import ForgeEngine
 from torchtitan.experiments.forge.job_config import ForgeJobConfig
-
-from forge.actors._torchstore_utils import DcpHandle, get_dcp_whole_state_dict_key
-
-from forge.controller import ForgeActor
-from forge.data.utils import batch_to_device
-from forge.observability.metrics import record_metric, Reduce
-from forge.observability.perf_tracker import Tracer
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -87,12 +91,6 @@ def cleanup_old_weight_versions(
                     logger.debug(f"Removed old weights at {item_path}")
                 except OSError as e:
                     logger.debug(f"Error deleting {item_path}: {e}")
-
-
-async def _parallel_put(kv_pairs: Iterable[tuple[str, Tensor]]):
-    keys, tensors = zip(*kv_pairs)
-    coros = [ts.put(key, tensor.detach().cpu()) for key, tensor in zip(keys, tensors)]
-    await asyncio.gather(*coros)
 
 
 @dataclass
@@ -337,7 +335,10 @@ class RLTrainer(ForgeActor):
             await ts.put(key, dcp_handle)
             t.step("dcp_save")
         else:
-            await _parallel_put(hf_state_dict.items())
+            for name, param in hf_state_dict.items():
+                key = get_param_key(policy_version, name)
+                await ts.put(key, param.detach().cpu())
+                t.step("ts_save")
             t.step("ts_save")
         t.stop()
         end_time = time.perf_counter()
