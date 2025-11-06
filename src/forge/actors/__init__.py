894c8a3101b1e423a0efc641aab4360276823b5c
Allen Wang <allencwang@fb.com>
2025-11-06 14:59:02 -0800

    rltrainer dep

--- src/forge/actors/__init__.py
+++ src/forge/actors/__init__.py
@@ -4,9 +4,12 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+import warnings
+
 __all__ = [
     "Generator",
     "TitanTrainer",
+    "RLTrainer",  # Deprecated, use TitanTrainer
     "ReplayBuffer",
     "ReferenceModel",
     "SandboxedPythonCoder",
@@ -22,6 +25,16 @@ def __getattr__(name):
         from .trainer import TitanTrainer
 
         return TitanTrainer
+    elif name == "RLTrainer":
+        warnings.warn(
+            "RLTrainer is deprecated and will be removed in a future version. "
+            "Please use TitanTrainer instead.",
+            FutureWarning,
+            stacklevel=2,
+        )
+        from .trainer import RLTrainer
+
+        return RLTrainer
     elif name == "ReplayBuffer":
         from .replay_buffer import ReplayBuffer
 
