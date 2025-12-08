fffb1181d4536bc76b0e0ee6f65695ed2c19449b
Danielle Pintz <daniellepintz@gmail.com>
2025-12-07 23:17:55 -0800

    upd

--- .meta/mast/env_setup.sh
+++ .meta/mast/env_setup.sh
@@ -6,6 +6,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+# Set up conda environment and install forge with mounting
+
 # Configuration
 CONDA_ENV_NAME="forge:314c3548ae691f4aa2e49f1b1fad06b3"
 
